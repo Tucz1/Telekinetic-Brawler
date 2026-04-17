ccc677b44788f99aa127901a1b7bfcccdff28455
LuukasMakela <luukas.makela@laajasalonopisto.fi>
2026-04-17 14:15:25 +0300

    Added audio FW plus audio test scene

---
+++ TelekineticBrawler/Assets/AudioFW/Scripts/Scripts for tests/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public string C, V, X;
+    public string Q, W;
+
+    Dictionary<KeyCode, string> bindings = new Dictionary<KeyCode, string>();
+
+    void Start()
+    {
+        bindings.Add(KeyCode.C, C);
+        bindings.Add(KeyCode.V, V);
+        bindings.Add(KeyCode.X, X);
+
+    }
+    void Update()
+    {
+        foreach (var kc in bindings.Keys) {
+            if (Input.GetKeyDown(kc))
+                AudioFW.Play(bindings[kc]);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+            AudioFW.PlayLoop(Q);
+        if (Input.GetKeyDown(KeyCode.W))
+            AudioFW.PlayLoop(W); 
+        if (Input.GetKeyDown(KeyCode.A))
+            AudioFW.StopLoop(Q);
+        if (Input.GetKeyDown(KeyCode.S))
+            AudioFW.StopLoop(W);
+    }
+}
