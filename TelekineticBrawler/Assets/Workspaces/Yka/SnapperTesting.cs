5bcfcb623f4e3d2293f91f143c7f5fcf4f670953
Yrjö Peussa <peussa@iki.fi>
2026-04-29 14:42:48 +0300

    Yka testing scene minor change

--- TelekineticBrawler/Assets/Workspaces/Yka/SnapperTesting.cs
+++ TelekineticBrawler/Assets/Workspaces/Yka/SnapperTesting.cs
@@ -1,22 +1,29 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Controls;
 
 public class SnapperTesting : MonoBehaviour {
     [SerializeField] Transform throwPoseAnchor;
     [SerializeField] Transform holdPoseAnchor;
     [SerializeField] Transform objectToSnap;
     [SerializeField] Transform holder;
-
+    [SerializeField] string upKey = "q";
+    [SerializeField] string downKey = "a";
+    
     void Update() {
-        if (Keyboard.current.qKey.wasPressedThisFrame) {
+        var upkeyControl = Keyboard.current.FindKeyOnCurrentKeyboardLayout(upKey);
+        // var upkeyControl = Keyboard.current.qKey;
+        
+        if (upkeyControl.wasPressedThisFrame) {
             var T = holder.rotation;
             var invSl = Quaternion.Inverse(throwPoseAnchor.localRotation);
             objectToSnap.rotation = T * invSl;
             var diff = throwPoseAnchor.position - objectToSnap.position;
             objectToSnap.position = holder.position - diff;
         }
-
-        if (Keyboard.current.aKey.wasPressedThisFrame) {
+        var downkeyControl = Keyboard.current.FindKeyOnCurrentKeyboardLayout(downKey);
+        
+        if (downkeyControl.wasPressedThisFrame) {
             var T = holder.rotation;
             var invSl = Quaternion.Inverse(holdPoseAnchor.localRotation);
             objectToSnap.rotation = T * invSl;
