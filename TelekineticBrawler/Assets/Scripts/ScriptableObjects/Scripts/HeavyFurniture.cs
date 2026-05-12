fa9f27e66171d23ba9515464845c4b12b0430e4d
Nicolas <nicolas.piirainen@gmail.com>
2026-05-12 22:43:08 +0300

    Added a shit ton of functional objects
    DO NOT RESIZE THEM.

---
+++ TelekineticBrawler/Assets/Scripts/ScriptableObjects/Scripts/HeavyFurniture.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Object", menuName = "Objects/HeavyFurniture")]
+class HeavyFurniture : WeaponData
+{
+	public override void UseAbility(AbilityInfo info)
+	{
+		
+		// Ability logic? Or maybe just a trigger?
+	}
+	
+	// Break consistent across all items & objects, so no need to declare?
+	
+}
---
+++ TelekineticBrawler/Assets/Scripts/ScriptableObjects/Scripts/LightFurniture.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Object", menuName = "Objects/LightFurniture")]
+class LightFurniture : WeaponData
+{
+	public override void UseAbility(AbilityInfo info)
+	{
+		
+		// Ability logic? Or maybe just a trigger?
+	}
+	
+	// Break consistent across all items & objects, so no need to declare?
+	
+}
